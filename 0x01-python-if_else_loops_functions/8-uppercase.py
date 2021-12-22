40c39e85d2bd89835d0839bd5bff3abf93a7b0b6
Neo <neochronos.chaos9@gmail.com>
2021-12-22 15:42:32 +0100

    Fixed files

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-
     for i in str:
-
-        if chr(i) > 97 and chr(i) < 123:
+        if ord(i) >= 97 and ord(i) < 123:
             i = ord(i) - 32
         else:
             pass
-
-print("{}".format(chr(i)))
+    print("{}".format(chr(i)))
