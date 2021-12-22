b00fbfdd7c4fb060d623652950d31b57f750b5a4
Neo <neochronos.chaos9@gmail.com>
2021-12-22 15:49:21 +0100

    Fixed 6 according to pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 for j in range(10):
     for i in range(10):
-        if i > j:
-            if j == 8:
-               print("{:d}{:d}\n".format(j, i), end="")
+        if(i > j):
+            if(j == 8):
+                print("{:d}{:d}\n".format(j, i), end="")
             else:
                 print("{:d}{:d}, ".format(j, i), end="")
-        elif i == j:
-            pass
         else:
             pass
