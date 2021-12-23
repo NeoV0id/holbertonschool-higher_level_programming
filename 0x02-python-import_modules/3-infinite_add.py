aea78648ca3797ba833db74a50b18c8919a2c277
Neo <neochronos.chaos9@gmail.com>
2021-12-23 21:52:45 +0100

    Added 5 and fixed 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ == '__main__':
-    n = 0
+    sum = 0
     for i in sys.argv[1:]:
-        n += int(i)
-    print("{}".format(n))
+        sum += int(i)
+    print(sum)
