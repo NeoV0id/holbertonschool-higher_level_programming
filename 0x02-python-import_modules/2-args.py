ecaa3fe8ce220f3b0000e7e3456a639b7eba8bf6
Neo <neochronos.chaos9@gmail.com>
2021-12-23 21:36:30 +0100

    Fixed 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    for i in range(1, len(argv)):
-        if n == 1:
-            print("{} argument :".format(len(argv)), end="")
-        else:
-            print("{} arguments :".format(len(argv)), end="")
+    import sys
 
-        print("{:d}: {:s}".format(i, str(argv)))
+    if len(sys.argv) - 1 == 0:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+
+    elif len(sys.argv) - 1 == 1:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+
+    if len(sys.argv) - 1 > 0:
+        j = 0
+        for i in sys.argv[1:]:
+            j += 1
+            print("{:d}: {:s}".format(j, i))
