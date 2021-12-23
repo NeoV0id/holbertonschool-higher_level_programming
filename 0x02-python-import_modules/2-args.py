ce15c2fe58c1de7fdfc18ba2ff44b2144fc51958
Neo <neochronos.chaos9@gmail.com>
2021-12-23 21:29:44 +0100

    Fixed according to pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
+import sys
 
 if __name__ == "__main__":
-    import sys
-    for i in range (1, len(argv)):
-        if n == 1:
-            print("{} argument :".format(len(argv)), end="")
-        else:
-            print("{} arguments :".format(len(argv)), end="")
-
-        print("{:d}: {:s}".format(i, str(argv)))
-
-else:
-    for i in range (1, len(argv)):
+    for i in range(1, len(argv)):
         if n == 1:
             print("{} argument :".format(len(argv)), end="")
         else:
