80cc459b8cb5b08e8697612232d1a8e42b5a5443
Neo <neochronos.chaos9@gmail.com>
2022-02-03 00:46:53 +0100

    Added 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,15 @@
-
+#!/usr/bin/python3
+def print_matrix_integer(matrix[[]]):
+    if type(matrix) is not list:
+        return
+    for i in matrix:
+        if type(i) is not list:
+            return
+        for j in i:
+            if type(j) is not integer:
+                return
+            elif j:
+                print(j, end="")
+            elif j != len(i)-1:
+                print(" ")
+        print()
