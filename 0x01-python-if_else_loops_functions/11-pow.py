3acdf7d40c3d9decb94b349b1cd351c11f1a0811
Neo <neochronos.chaos9@gmail.com>
2022-02-02 16:24:57 +0100

    Fixed 11-pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -3,7 +3,11 @@ def pow(a, b):
     if a < 0:
         a = a * -1
         result = a ** b
-        result = result * -1
+        if a < b:
+            result = result * -1
+    elif a < 0 and b < 0:
+        a = a * -1
+        result = a ** b
     else:
         result = a ** b
 
