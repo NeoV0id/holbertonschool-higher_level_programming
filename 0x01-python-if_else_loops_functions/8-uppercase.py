577676b22acbfad2bbfbf14945c77ba5c53f6f1a
Neo <neochronos.chaos9@gmail.com>
2022-02-02 16:01:46 +0100

    Fixed 8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= 97 and ord(i) < 123:
+        if ord(i) >= 97 and ord(i) <= 123:
             i = ord(i) - 32
-            print("{}".format(chr(i)), end="")
-        else:
-            print("{}".format(i), end="")
+            i = chr(i)
+        print("{}".format(i), end="")
+    print("\n")
