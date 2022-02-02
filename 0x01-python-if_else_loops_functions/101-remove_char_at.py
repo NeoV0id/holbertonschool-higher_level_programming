1f112848205aa2d98243ea77d6a2e1786c15e3ee
Neo <neochronos.chaos9@gmail.com>
2022-02-02 17:02:43 +0100

    Fixed 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,7 @@ def remove_char_at(str, n):
     if n < 0:
         return str
     else:
-        for i in str:
+        for i in range(0, len(str)):
             if i == n:
                 i = ''
     return str
