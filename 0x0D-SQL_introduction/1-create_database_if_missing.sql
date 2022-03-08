955c2ea5979b3e807b65ff647ed06ac40364792b
NeoV0id <neo@LAPTOP-M3VPT85L.localdomain>
2022-03-08 23:09:54 +0100

    Fixed 1 and 2

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- This script will create the database hbtn_0c_0 on a server --
 -- hbtn_0c_0 should have root privilege --
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- This script will delete the database hbtn_0c_0 from a server --
 -- hbtn_0c_0 --
-DROP DATABASE [IF EXISTS] hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
