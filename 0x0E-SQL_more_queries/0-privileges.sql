2ad858da825ba59792f06a10f4e0995556454879
NeoV0id <neov0id.nova@gmail.com>
2022-03-09 12:23:47 -0800

    Modified 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- This script will list all privileges for users --
 -- user_0d_1 and user_0d_2 on localhost --
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
