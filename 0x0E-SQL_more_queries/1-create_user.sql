938c4697f1926db53b94d5537e7dff727531d9f4
NeoV0id <neov0id.nova@gmail.com>
2022-03-09 12:26:41 -0800

    Modified 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- This script will create a server user --
 -- user_0d_1 --
-GRANT ALL PRIVILEGES IF NOT EXISTS TO user_0d_1 IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES IF NOT EXISTS TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
