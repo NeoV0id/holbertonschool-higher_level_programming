5e5df66620391cae2bd8bcb365a626aa492a7bc3
NeoV0id <neo@LAPTOP-M3VPT85L.localdomain>
2022-03-09 17:01:36 +0100

    Modified 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,7 @@
 -- This script will create a table --
 -- first_table --
-CREATE TABLE IF NOT EXIST first_table(id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXIST first_table(
+	id INT, 
+	name VARCHAR (256)
+);
 
