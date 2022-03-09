51fb3181f4628a341bb6dbb45b7680b146d1c396
NeoV0id <neov0id.nova@gmail.com>
2022-03-09 14:29:22 -0800

    Added 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- This script will list all genres of the show Dexter --
+-- tv_genres --
+SELECT tv_genres.name AS 'name'
+FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;
+
