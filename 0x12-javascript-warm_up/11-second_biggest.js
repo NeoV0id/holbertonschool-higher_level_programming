1c1a5ebb1c382fbbf9b31e2d9b9951bdba599670
NeoV0id <jokerghoul.kpop@gmail.com>
2022-04-05 11:16:19 -0700

    Semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 
-if (argv.$1 === null){
-	console.log (0);
-}else if (argv.$2 === null){
-	console.log (0);
-}else{
-	for (let i = 0; i < argv.length; i++){
-		if (argv.i > argv.i + 1){
-			let largest = argv.i;
-			if (argv.i + 1 > argv.i + 2){
-				let result = argv.i + 1;
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log(0);
+} else if (process.argv[3] === undefined || isNaN(process.argv[3])) {
+  console.log(0);
+} else {
+  for (let i = 0; i < process.argv.length; i++) {
+    if (process.argv[i] > process.argv[i + 1]) {
+      const largest = process.argv.i;
+      if (process.argv[i + 1] > process.argv[i + 2]) {
+        const result = process.argv[i + 1];
+      }
+    }
+  }
+}
