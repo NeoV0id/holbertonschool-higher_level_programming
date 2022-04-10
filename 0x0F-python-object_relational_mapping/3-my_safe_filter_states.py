f53078b44bf007d0d2fb10bbcc7dd293e3eb280d
NeoV0id <neov0id.nova@gmail.com>
2022-04-10 13:26:06 -0700

    Added tested files

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,13 +8,16 @@ import sys
 
 if __name__ == '__main__':
 
-    conn = MySQLdb.connect(
+    db = MySQLdb.connect(
             host="localhost", port=3306, user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-            print(row)
-            cur.close()
-            conn.close()
+
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name=%s\
+            ORDER BY states.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        cur.close()
+        db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,13 +3,22 @@
 
 
 import MySQLdb
+import sys
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
-query_rows = cur.fetchall()
-for row in query_rows:
-            print(row)
-                    cur.close()
-                            conn.close()
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+            host='localhost', port=3306, user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        cur.close()
+        db.close()
