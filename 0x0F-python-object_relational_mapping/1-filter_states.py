f6d2aa73ca57ab036d39aa14ad3d1584e532e41b
NeoV0id <neov0id.nova@gmail.com>
2022-04-10 10:23:24 -0700

    Fix first files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,13 +3,19 @@
 
 
 import MySQLdb
+import sys
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
+if __name__ == '__main__':
+
+
+conn = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 cur = conn.cursor()
 cur.execute("SELECT * FROM states WHERE N IN name ORDER BY id ASC")
 query_rows = cur.fetchall()
 for row in query_rows:
-        print(row)
-        cur.close()
-        conn.close()
+    print(row)
+    cur.close()
+    conn.close()
