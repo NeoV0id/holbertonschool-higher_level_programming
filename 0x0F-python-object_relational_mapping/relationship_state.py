fb5c2c13104e2a29b242203fc9f3e1f1968c91e5
NeoV0id <neov0id.nova@gmail.com>
2022-04-10 14:59:51 -0700

    pycode style

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -6,9 +6,9 @@ from relationship_city import Base, City
 
 
 class State(Base):
-        """ Represents a State """
-            __tablename__ = "states"
-            id = Column(Integer, autoincrement=True, unique=True,
-                            nullable=False, primary_key=True)
-            name = Column(String(128), nullable=False)
-            cities = relationship("City", backref="state", cascade="all, delete")
+""" Represents a State """
+    __tablename__ = "states"
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
