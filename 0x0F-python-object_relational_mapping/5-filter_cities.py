ef160803e96e33762cb02192d6714bc8a27559ec
NeoV0id <neov0id.nova@gmail.com>
2022-04-10 14:05:25 -0700

    Modified

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     conn = MySQLdb.connect(
             host='localhost', port=3306, user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3])
+            passwd='', db=sys.argv[3])
 
     cur = conn.cursor()
     cur.execute("SELECT 'cities'.'name'\
