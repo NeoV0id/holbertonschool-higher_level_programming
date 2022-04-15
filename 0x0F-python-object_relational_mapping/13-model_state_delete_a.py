d917c99e95c597fb4281ca3f7ed4a7282268a9db
NeoV0id <neov0id.nova@gmail.com>
2022-04-15 14:54:48 -0700

    Minor change task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,6 +18,9 @@ if __name__ == "__main__":
     for state in session.query(State):
         if "a" in state.name:
             session.delete(state)
+            continue
+        else:
+            continue
     session.commit()
 
     session.close()
