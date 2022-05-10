169a16dc063876eaffc41efbef17ac2dc7273279
NeoV0id <neov0id.nova@gmail.com>
2022-05-10 00:05:04 -0700

    pycodestyle

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import sys
 
     req = requests.get('https://api.github.com/user',
-            auth=(sys.argv[1], sys.argv[2]))
+                       auth=(sys.argv[1], sys.argv[2]))
     if req.status_code >= 400:
         print('None')
     else:
